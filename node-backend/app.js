f0feef39a79044bcae219d366605aa0f0cb06eaa
shiva-kumar-nadigoti <46747378+NSK-007@users.noreply.github.com>
2023-02-26 18:23:03 +0530

    Update app.js

--- node-backend/app.js
+++ node-backend/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const router = require('./routes/expense-router');
+const sequelize = require('./util/database');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json({extended:false}));
 
+app.use(router);
 
-
-app.listen(8888);
+sequelize
+    .sync()
+    .then(() => {
+        app.listen(8888);
+    })
+    .catch(err => {
+        console.log(err)
+    });
